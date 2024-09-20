37d533c8b76c563310bac76971761ac57d1c5032
Alex <alexwright@45177@gmail.com>
2024-09-20 12:19:02 -0400

    Initial commit

---
+++ .py
@@ -0,0 +1,10 @@
+student = "Alex Wright"
+major = "Information Technology"
+creditHours = 16
+
+favoriteClass = input("Please Enter Your Favorite Class: ")
+
+print(f"Student Name: {student}")
+print(f"Major: {major}")
+print(f"Credit Hours: {creditHours}")
+print(f"Favorite Class: {favoriteClass}")
